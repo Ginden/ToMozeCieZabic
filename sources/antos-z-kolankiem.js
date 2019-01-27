5f97b40a9f59b9c23afdb85381de1d56adf5ea5d
Michał Wadas <michalwadas@gmail.com>
2019-01-27 20:21:41 +0100

    Antoś gotowy na salony

--- sources/antos-z-kolankiem.js
+++ sources/antos-z-kolankiem.js
@@ -9,10 +9,16 @@ module.exports = {
         name: 'Jędrzej Dzięcioł',
         title: 'doktor nauk habilitowanych, akupunkturzysta'
     },
-    question: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    question: `Panie doktorze! Antoś przewrócił się na placu zabaw, a jego malutkie kolanko wygięło się w złą stronę! Teraz nie chodzi i ciągle płacze.
+    Nie chcę iść do przychodni, bo ostatnim razem pielęgniarka groziła nam sanepidem za nieszczepienie dziecka. Jakie naturalne panaceum mogę zastosować?`,
+    answer: [
+        `Szanowna Pani!`,
+        `Przeprost kolana to dolegliwość z którą można poradzić sobie w domu.
+        Zalecam więc taki reżim leczenia: smarować kolano co trzy godziny olejkiem CBD bez GMO.
+    Olejek CBD może kupić Pani w moim sklepie z suplementami.`,
+        `Proszę nie godzić się na gips - lekarze dodają do niego żelatyny, rzekomo dla poprawy parametrów mechanicznych.
+        Żelatynę pozyskuje się m. in. z krów, więc nie można wykluczyć że spowodowałaby ona u Antosia chorobę wściekłych krów.
+        Gips może Pani zastąpić naturalnymi okładami z liści organicznej kapusty.`
+    ],
     template: 'qa'
 };
